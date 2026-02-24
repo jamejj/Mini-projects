29263b13ceb0e839e2007724ab1e344ae03fb064
jamejj <jjaworskyy@gmail.com>
2026-02-24 19:48:40 +0100

    Task Adding

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -1,9 +1,38 @@
-const addButton = document.getElementById('addTask')
-const showTaskInput = document.getElementById('secondContainer')
+const addButton = document.getElementById('addTask');
 
-addButton.addEventListener('click', addTask)
+const showTaskInput = document.getElementById('secondContainer');
+const closeSecondContainer = document.getElementById('closeTaskInput');
+let clearInput = document.getElementById('inputTask');
 
+const list = document.getElementById('list');
+const addToList = document.getElementById('addTaskToList')
+
+addButton.addEventListener('click', addTask);
+closeSecondContainer.addEventListener('click', closeTaskInput);
+addToList.addEventListener('click', addTaskToList)
 function addTask(){
     showTaskInput.style.visibility = "visible";
-    alert('działa')
 }
+
+function closeTaskInput(){
+    showTaskInput.style.visibility = "hidden";
+    clearInput.value = "";
+}
+
+function addTaskToList(){
+    const text = clearInput.value.trim();
+    if (text === '')
+        return;
+
+    const li = document.createElement('li');
+    li.textContent = text;
+
+    list.appendChild(li);
+
+  /*  closeTaskInput();*/
+
+}
+
+
+
+
