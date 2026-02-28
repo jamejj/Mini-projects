436d85de79bf51558370ca4d856c38eb5067404e
jamejj <jjaworskyy@gmail.com>
2026-03-01 00:29:45 +0100

    JS Calculate

---
+++ OneRepMaxCalculator/script.js
@@ -0,0 +1,22 @@
+function calculate(event){
+
+    const maxWeight = parseFloat(document.getElementById('weight').value);
+    const maxReps = parseFloat(document.getElementById('reps').value);
+    const result = document.getElementById('result');
+
+
+    if(isNaN(maxWeight) || isNaN(maxReps)){
+        alert('Please enter valid numbers');
+        return;
+    }
+
+    let oneRepMax;
+    if(maxReps === 1){
+        oneRepMax = maxWeight
+    }else{
+        oneRepMax = maxWeight*(1+(0.0333*maxReps));
+    }
+
+    result.textContent = oneRepMax.toFixed(2) + 'kg';
+
+}
