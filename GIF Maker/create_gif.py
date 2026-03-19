21fbe1632f884a9cc65fa425bec977bcf73ccf1c
jamejj <jjaworskyy@gmail.com>
2026-03-19 19:20:24 +0100

    GIF Maker

---
+++ GIF Maker/create_gif.py
@@ -0,0 +1,11 @@
+import imageio.v3 as iio      #reader and writer of image data
+
+print(iio)
+
+filenames = ['img_1.png', 'img_2.png','img_3.png','img_4.png','img_5.png']
+images = []
+
+for filename in filenames:
+    images.append(iio.imread(filename))
+
+iio.imwrite('GIF.gif', images, duration = 300)
